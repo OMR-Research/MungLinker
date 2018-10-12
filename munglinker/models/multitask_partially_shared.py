fdd660bb905a78d308d1b82d1a50a657c0a89b51
Jan Hajic Jr <hajicj@ufal.mff.cuni.cz>
2018-10-12 11:48:24 +0200

    Adding skeleton files for multitask models

--- munglinker/models/multitask_partially_shared.py
+++ munglinker/models/multitask_partially_shared.py
@@ -1,7 +1,11 @@
-"""This module implements a class that..."""
+#!/usr/bin/env python
+"""This is a script that..."""
 from __future__ import print_function, unicode_literals
-
 import logging
+
+__version__ = "0.0.1"
+__author__ = "Jan Hajic jr."
+
 
 import numpy as np
 
@@ -11,119 +15,18 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 
-__version__ = "0.0.1"
-__author__ = "Jan Hajic jr."
-
-
 MIDI_MAX_LEN = 256
 MIDI_N_PITCHES = 128
-
 
 BATCH_SIZE = 4
 
 
-class BaseConvnet(nn.Module):
-    """Onset counter recurrent network.
-
-    Heavily draws from:
-
-    ``https://github.com/meijieru/crnn.pytorch/blob/ab61ff13c204a03d08b2836f39688c851f50fb12/models/crnn.py``
-
-    It is a sequence-to-sequence model that in each step outputs a number of onsets
-    and the loss is computed from their sum.
-
-    The RNN produces in each step a class: how many onsets it thinks there
-    are added from the current frame. There are at most ``MAX_N_SIMULTANEOUS_ONSETS``.
-    """
-    def __init__(self,
-                 n_input_channels=3,
-                 leaky_relu=False):
-        super(BaseConvnet, self).__init__()
-
-        self.n_input_channels = n_input_channels
-
-        # Convolutional part
-        # ------------------
-
-        # Conv layer params, downstream through the stack
-        kernel_sizes = [3, 3, 3, 3, 3]
-        paddings = [1, 1, 1, 1, 1]
-        strides = [1, 1, 1, 1, 1]
-        n_filters = [8, 16, 32, 32, 32]
-
-        n_pools = 5
-
-        cnn = nn.Sequential()
-
-        def convRelu(i, batchNormalization=True):
-            """Builds the i-th convolutional layer/batch-norm block."""
-            layer_n_input_channels = n_input_channels
-            if i != 0:
-                layer_n_input_channels = n_filters[i - 1]
-            layer_n_output_channels = n_filters[i]
-            cnn.add_module('conv{0}'.format(i),
-                           nn.Conv2d(layer_n_input_channels,
-                                     layer_n_output_channels,
-                                     kernel_sizes[i],
-                                     strides[i],
-                                     paddings[i]))
-            if batchNormalization:
-                cnn.add_module('batchnorm{0}'.format(i),
-                               nn.BatchNorm2d(layer_n_output_channels))
-            if leaky_relu:
-                cnn.add_module('relu{0}'.format(i),
-                               nn.LeakyReLU(0.2, inplace=True))
-            else:
-                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
-
-        # Input size expected (n_batch x n_channels x MIDI_N_PITCHES x MIDI_MAX_LEN),
-        # which is by default (n_batch x 3 x 256 x 512)
-        convRelu(0)
-        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 8 x 128 x 256
-        convRelu(1)
-        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 16 x 64 x 128
-        convRelu(2)
-        cnn.add_module('pooling{0}'.format(2), nn.MaxPool2d(2, 2))  # 32 x 32 x 64
-        convRelu(3)
-        cnn.add_module('pooling{0}'.format(3), nn.MaxPool2d(2, 2))  # 32 x 16 x 32
-        convRelu(4)
-        cnn.add_module('pooling{0}'.format(4), nn.MaxPool2d(2, 2))  # 32 x 8 x 16
-
-        self.cnn = cnn
-
-        # Add fully connected layer stack
-        fcn = nn.Sequential()
-        fcn.add_module('fcn0',
-                       nn.Linear(in_features=32 * 8 * 16,
-                                 out_features=2,  # Output decision
-                                 bias=False))
-
-        self.fcn = fcn
-
-        # Output through softmax
-        self.softmax = nn.Softmax()
-
-    def get_conv_output(self, input):
-        conv_output = self.cnn(input)
-        return conv_output
-
-    def get_fcn_output_from_conv_output(self, conv_output):
-        fcn_input = conv_output.view(-1, 32 * 8 * 16)
-        fcn_output = self.fcn(fcn_input)
-        return fcn_output
-
-    def get_output_from_fcn_output(self, fcn_output):
-        return self.softmax(fcn_output)
-
-    def forward(self, input):
-        conv_output = self.get_conv_output(input)
-        fcn_output = self.get_fcn_output_from_conv_output(conv_output)
-        output = self.get_output_from_fcn_output(fcn_output)
-        return output
+class MultitaskPartiallyShared(nn.Module):
+    raise NotImplementedError()
 
 
 def get_build_model():
-    return BaseConvnet
+    return MultitaskPartiallyShared
 
 
 ##############################################################################
@@ -132,6 +35,9 @@ def get_build_model():
 def prepare_patch_and_target(mungos_from, mungos_to, patches, targets,
                              target_is_onehot=True):
     """Does not do anything to patches.
+
+    For the multitask learning, we intend to use the class names of mungos_from
+    and mungos_to (which is why the data pool is giving these to us).
 
     :param target_is_onehot: Expands targets to two-way softmax format.
     """
@@ -221,8 +127,8 @@ if __name__ == '__main__':
     patch_width = 512
     patch_channels = 3
 
-    model = BaseConvnet(n_input_channels=patch_channels,
-                        leaky_relu=False)
+    model = MultitaskFullyShared(n_input_channels=patch_channels,
+                                 leaky_relu=False)
 
     # Prepare dummy batch
     # -------------------
