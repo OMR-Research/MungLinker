e10b5694ae51ef2101606359f1e5dff868ab39d2
Alexander Pacha <alexander.pacha@gmail.com>
2019-03-07 09:15:36 +0100

    Adding script to automatically download muscima++ dataset incl. all images. Updated readme to reflect new way of preparing dataset

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# munglinker
+# MuNG Linker
 
-Experiments on linking the nodes of a music notation graph (MuNG).
+Experiments on linking the nodes of a music notation graph (MuNG). The basic idea is to machine-learn the relations from images between objects in a notation graph.
 
 ## Requirements
 
 - Python 3.6
 - [PyTorch](https://pytorch.org/)
 
-Install with ``pip install -e .`` to follow updates without having to re-install.
+Install the sources with ``pip install -e .`` to follow updates without having to re-install.
 
-Download [MUSCIMA++ dataset](https://lindat.mff.cuni.cz/repository/xmlui/handle/11372/LRT-2372) and extract the xml-files from `data/cropobjects_withstaff` into `data/mungs`.
+## Dataset
+Download the MUSCIMA++ dataset with the required images, by running 
 
-Don't download [CVC-MUSCIMA](http://www.cvc.uab.es/cvcmuscima/index_database.html) dataset, but just download and extract the used images from [here](https://owncloud.tuwien.ac.at/index.php/s/Xv91caXnPubL6Zk/download) into `data/images`
+`python munglinker/prepare_dataset.py`
 
+or manually download the [MUSCIMA++ dataset](https://lindat.mff.cuni.cz/repository/xmlui/handle/11372/LRT-2372) and extract the xml-files from `data/cropobjects_withstaff` into `data/mungs` as well as the used images from the [CVC-MUSCIMA](http://www.cvc.uab.es/cvcmuscima/index_database.html) dataset from [here](https://github.com/apacha/OMR-Datasets/releases/download/datasets/CVC_MUSCIMA_PP_Annotated-Images.zip) into `data/images`
+
+## Training
 Run the training by calling:
 
 `python munglinker/train.py`
---
+++ munglinker/prepare_dataset.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+import shutil
+from distutils.dir_util import copy_tree
+
+from omrdatasettools.downloaders.MuscimaPlusPlusDatasetDownloader import MuscimaPlusPlusDatasetDownloader
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--mung_root', action='store', default="../data/mungs",
+                        help='The root directory that will contain the MuNG XMLs.')
+    parser.add_argument('-i', '--image_root', action='store', default="../data/images",
+                        help='The root directory that will contain the images of'
+                             ' scores that are represented by the MuNGs. The'
+                             ' image names must correspond to the MuNG file'
+                             ' names, up to the file type suffix.')
+    args = parser.parse_args()
+    mung_root_directory = args.mung_root
+    image_root_directory = args.image_root
+
+    os.makedirs(mung_root_directory, exist_ok=True)
+    os.makedirs(image_root_directory, exist_ok=True)
+
+    temporary_directory = "temp"
+    dataset_downloader = MuscimaPlusPlusDatasetDownloader()
+    dataset_downloader.download_and_extract_dataset(temporary_directory)
+
+    copy_tree(os.path.join(temporary_directory, "v1.0", "data", "cropobjects_withstaff"), mung_root_directory)
+    copy_tree(os.path.join(temporary_directory, "v1.0", "data", "images"), image_root_directory)
+
+    shutil.rmtree(temporary_directory)
+    os.remove(dataset_downloader.get_dataset_filename())
+    os.remove(dataset_downloader.get_imageset_filename())
+
