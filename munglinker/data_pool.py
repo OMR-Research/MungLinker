74df5b723cde287c2c1666c7b877c3711ddd91b2
Jonas Schäfer <44198728+j0ner0n@users.noreply.github.com>
2021-02-09 08:24:43 +0100

    Data loading fix (#5)
    * Fixed munglinker loading issue
    
    * Wrong method name caused mung generator to crash previously
    
    * Another renaming of a method of the MuNG repository

--- munglinker/data_pool.py
+++ munglinker/data_pool.py
@@ -104,7 +104,7 @@ class PairwiseMungoDataPool(Dataset):
             patch = self.load_patch(image_index, mungo_from, mungo_to)
             patches_batch[i_entity] = patch
 
-            mungos_are_connected = mungo_to.objid in mungo_from.outlinks
+            mungos_are_connected = mungo_to.id in mungo_from.outlinks
             if mungos_are_connected:
                 targets[i_entity] = 1
 
@@ -148,7 +148,7 @@ class PairwiseMungoDataPool(Dataset):
         number_of_samples = 0
         for mung_index, mung in enumerate(tqdm(self.mungs, desc="Loading MuNG-pairs")):
             object_pairs = self.get_all_neighboring_object_pairs(
-                mung.cropobjects,
+                mung.vertices,
                 max_object_distance=self.max_edge_length,
                 grammar=self.grammar)
             for (m_from, m_to) in object_pairs:
@@ -402,11 +402,11 @@ def __load_munglinker_data(mung_root: str, images_root: str,
         exclude_classes = {}
 
     all_mung_files = glob(mung_root + "/**/*.xml", recursive=True)
-    mung_files_in_this_split = [f for f in all_mung_files if os.path.splitext(os.path.basename(f))[0] in include_names]
+    mung_files_in_this_split = sorted([f for f in all_mung_files if os.path.splitext(os.path.basename(f))[0] in include_names])
 
     all_image_files = glob(images_root + "/**/*.png", recursive=True)
-    image_files_in_this_split = [f for f in all_image_files if
-                                 os.path.splitext(os.path.basename(f))[0] in include_names]
+    image_files_in_this_split = sorted([f for f in all_image_files if
+                                 os.path.splitext(os.path.basename(f))[0] in include_names])
 
     mungs = []
     images = []
